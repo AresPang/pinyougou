f539a2bc8a9cc325b7a0116fb6bf9b6f15221403
AresPang <aresponter@163.com>
2019-05-23 19:58:16 +0800

    Brand requirement finished!

--- pinyougou/pinyougou-parent/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou/pinyougou-parent/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -1,9 +1,51 @@
 package com.pinyougou.sellergoods.service;
 
 import com.pinyougou.pojo.TbBrand;
+import entity.PageResult;
 
 import java.util.List;
 
 public interface BrandService {
     public List<TbBrand> findAll();
+
+    /**
+     * 品牌分页
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    public PageResult findPage(int pageNum,int pageSize);
+
+    /**
+     * 增加品牌
+     * @param tbBrand
+     */
+    public void add(TbBrand tbBrand);
+
+    /**
+     * 根据主键查品牌
+     * @param id
+     * @return
+     */
+    public TbBrand findOne(Long id);
+
+    /**
+     * 修改
+     * @param brand
+     */
+    public void update(TbBrand brand);
+
+    /**
+     * 根据品牌id删除品牌
+     * @param ids
+     */
+    public void delete(Long[] ids);
+
+    /**
+     * 带条件品牌分页
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    public PageResult findPage(TbBrand brand,int pageNum,int pageSize);
 }
